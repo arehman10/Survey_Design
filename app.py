c962fdd3030da0ce35652a648790110fd95b6b77
arehman10 <139470453+arehman10@users.noreply.github.com>
2025-04-24 12:03:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ import base64
 import streamlit.components.v1 as components
 from datetime import datetime
 
+import base64, tempfile, html
+from weasyprint import HTML          # pip install weasyprint
+
 # If using GLPK:
 import swiglpk as glpk
 
